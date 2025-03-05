fe58a56434f88f6b9e97ad0c8eab933438ec4f60
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2025-03-05 23:14:29 +0600

    shopping views routes added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 import AuthLayout from './components/auth/layout';
 import AuthLogin from './pages/auth/login';
@@ -10,6 +10,10 @@ import AdminOrders from './pages/admin-view/orders';
 import AdminFeatures from './pages/admin-view/features';
 import ShoppingLayout from './components/shopping-view/layout';
 import NotFound from './pages/not-fount';
+import ShoppingHome from './pages/shopping-view/home';
+import ShoppingListing from './pages/shopping-view/listing';
+import ShoppingCheckout from './pages/shopping-view/checkout';
+import ShoppingAccount from './pages/shopping-view/account';
 
 function App() {
 	return (
@@ -27,7 +31,13 @@ function App() {
 					<Route path='orders' element={<AdminOrders />} />
 					<Route path='features' element={<AdminFeatures />} />
 				</Route>
-				<Route path='/shop' element={<ShoppingLayout />}></Route>
+				<Route path='/shop' element={<ShoppingLayout />}>
+					<Route index element={<Navigate to='home' />} />
+					<Route path='home' element={<ShoppingHome />} />
+					<Route path='listing' element={<ShoppingListing />} />
+					<Route path='checkout' element={<ShoppingCheckout />} />
+					<Route path='account' element={<ShoppingAccount />} />
+				</Route>
 				<Route path='*' element={<NotFound />} />
 			</Routes>
 		</div>
