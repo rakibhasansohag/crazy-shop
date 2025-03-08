aa4e2949dd9dc33dd0139c476546044671d941dd
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2025-03-08 21:59:13 +0600

    skeletion ui added and updated logout functionlity

--- client/src/components/admin-view/header.tsx
+++ client/src/components/admin-view/header.tsx
@@ -1,18 +1,19 @@
 import { AlignJustify, LogOut } from 'lucide-react';
 import { Button } from '../ui/button';
-// import { useDispatch } from 'react-redux';
-// import { logoutUser } from '@/store/auth-slice';
+import { useDispatch } from 'react-redux';
+import { logoutUser } from '@/store/auth-slice';
+import { AppDispatch } from '../../store/store';
 
 type AdminHeaderProps = {
 	setOpen: (open: boolean) => void;
 };
 
 function AdminHeader({ setOpen }: AdminHeaderProps) {
-	// const dispatch = useDispatch();
+	const dispatch = useDispatch<AppDispatch>();
 
-	// function handleLogout() {
-	// 	dispatch(logoutUser());
-	// }
+	function handleLogout() {
+		dispatch(logoutUser());
+	}
 
 	return (
 		<header className='flex items-center justify-between px-4 py-3 bg-background border-b'>
@@ -22,7 +23,7 @@ function AdminHeader({ setOpen }: AdminHeaderProps) {
 			</Button>
 			<div className='flex flex-1 justify-end'>
 				<Button
-					// onClick={handleLogout}
+					onClick={handleLogout}
 					className='inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow'
 				>
 					<LogOut />
