c21f3d536488ab3b13a7b078c353f1f8c0365975
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2025-03-09 17:47:07 +0600

    image uploader components added

--- client/src/pages/admin-view/products.tsx
+++ client/src/pages/admin-view/products.tsx
@@ -9,6 +9,7 @@ import {
 	SheetTitle,
 } from '../../components/ui/sheet';
 import { addProductFormElements } from '../../config';
+import ProductImageUpload from '../../components/admin-view/image-upload';
 
 const initialFormData: Record<string, any> = {
 	image: null,
@@ -26,7 +27,9 @@ const AdminProducts = () => {
 	const [openCreateProductsDialog, setOpenCreateProductsDialog] =
 		useState(false);
 	const [formData, setFormData] = useState(initialFormData);
-
+	const [imageFile, setImageFile] = useState(null);
+	const [uploadedImageUrl, setUploadedImageUrl] = useState('');
+	const [imageLoadingState, setImageLoadingState] = useState(false);
 	const [currentEditedId, setCurrentEditedId] = useState(null);
 
 	function onSubmit(event: React.FormEvent<HTMLFormElement>) {
@@ -40,7 +43,19 @@ const AdminProducts = () => {
 					Add New Product
 				</Button>
 			</div>
-			<div className='grid gap-4 md:grid-cols-3 lg:grid-cols-4'></div>
+			<div className='grid gap-4 md:grid-cols-3 lg:grid-cols-4'>
+				{/* {productList && productList.length > 0
+					? productList.map((productItem) => (
+							<AdminProductTile
+								setFormData={setFormData}
+								setOpenCreateProductsDialog={setOpenCreateProductsDialog}
+								setCurrentEditedId={setCurrentEditedId}
+								product={productItem}
+								handleDelete={handleDelete}
+							/>
+					  ))
+					: null} */}
+			</div>
 			<Sheet
 				open={openCreateProductsDialog}
 				onOpenChange={() => {
@@ -55,7 +70,15 @@ const AdminProducts = () => {
 							{currentEditedId !== null ? 'Edit Product' : 'Add New Product'}
 						</SheetTitle>
 					</SheetHeader>
-
+					<ProductImageUpload
+						imageFile={imageFile}
+						setImageFile={setImageFile}
+						uploadedImageUrl={uploadedImageUrl}
+						setUploadedImageUrl={setUploadedImageUrl}
+						setImageLoadingState={setImageLoadingState}
+						imageLoadingState={imageLoadingState}
+						isEditMode={currentEditedId !== null}
+					/>
 					<div className='py-6'>
 						<CommonForm
 							onSubmit={onSubmit}
@@ -63,6 +86,7 @@ const AdminProducts = () => {
 							setFormData={setFormData}
 							buttonText={currentEditedId !== null ? 'Edit' : 'Add'}
 							formControls={addProductFormElements}
+							// isBtnDisabled={!isFormValid()}
 						/>
 					</div>
 				</SheetContent>
