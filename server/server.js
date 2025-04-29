82d4780406d74c26dffd55b5076e9947d89790b6
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2025-04-30 01:31:59 +0600

    shop order related routes and controller added

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const adminProductsRouter = require('./routes/admin/products-routes');
 const shopProductsRouter = require('./routes/shop/products-routes');
 const shopCartRouter = require('./routes/shop/cart-routes.js');
 const shopAddressRouter = require('./routes/shop/address-routes.js');
+const shopOrderRouter = require('./routes/shop/order-routes.js');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -60,6 +61,7 @@ app.use('/api/admin/products', adminProductsRouter);
 app.use('/api/shop/products', shopProductsRouter);
 app.use('/api/shop/cart', shopCartRouter);
 app.use('/api/shop/address', shopAddressRouter);
+app.use('/api/shop/order', shopOrderRouter);
 
 
 // Error handling middleware
