4afb9a5af3f505547fcca7dfec43c665b6db23f0
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2025-05-11 00:50:14 +0600

    dashboard features image page added

--- client/src/pages/admin-view/dashboard.tsx
+++ client/src/pages/admin-view/dashboard.tsx
@@ -1,5 +1,37 @@
-const AdminDashboard = () => {
-	return <div>AdminDashboard</div>;
-};
+import ProductImageUpload from '@/components/admin-view/image-upload';
+import { Button } from '@/components/ui/button';
+
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../../store/store';
+import { useState } from 'react';
+
+function AdminDashboard() {
+	const [imageFile, setImageFile] = useState<File | null>(null);
+	const [uploadedImageUrl, setUploadedImageUrl] = useState('');
+	const [imageLoadingState, setImageLoadingState] = useState(false);
+	const dispatch = useDispatch<AppDispatch>();
+
+	console.log(dispatch, 'dispatch');
+
+	const handleUploadFeatureImage = () => {};
+
+	return (
+		<div>
+			<ProductImageUpload
+				imageFile={imageFile}
+				setImageFile={setImageFile}
+				uploadedImageUrl={uploadedImageUrl}
+				setUploadedImageUrl={setUploadedImageUrl}
+				setImageLoadingState={setImageLoadingState}
+				imageLoadingState={imageLoadingState}
+				isCustomStyling={true}
+				isEditMode={false}
+			/>
+			<Button onClick={handleUploadFeatureImage} className='mt-5 w-full'>
+				Upload
+			</Button>
+		</div>
+	);
+}
 
 export default AdminDashboard;
